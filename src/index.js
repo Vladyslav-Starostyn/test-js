6208f9e15d3dee7c0e7c6b4931bfbfeb56554826
Vladyslav-Starostyn <starostin.vlad.od@gmail.com>
2022-06-15 14:14:50 +0300

    -----

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
 console.log("hello");
+const a = 12;
+console.log(a);
+
+const apples = 47;
+const grapes = 135;
+const total = apples + grapes;
+console.log(total);
+const diff = grapes - apples;
+console.log(diff);
+
+let students = 100;
+students += 510;
+console.log(students);
+
+const result = 108 + 223 - 2 * 5;
+console.log(result);
