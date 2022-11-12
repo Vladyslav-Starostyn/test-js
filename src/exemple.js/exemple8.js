c855b6c5b7d90bdec3eacdea0ad5b85664660ec1
Vladyslav-Starostyn <starostin.vlad.od@gmail.com>
2022-11-13 00:41:20 +0200

    8

--- src/exemple.js/exemple8.js
+++ src/exemple.js/exemple8.js
@@ -3,16 +3,86 @@
 // Коллекция объектов для всех примеров с автомобилями
 
 // const cars = [
-//   { make: 'Honda', model: 'CR-V', type: 'suv', amount: 14, price: 24045, onSale: true },
-//   { make: 'Honda', model: 'Accord', type: 'sedan', amount: 2, price: 22455, onSale: true },
-//   { make: 'Mazda', model: 'Mazda 6', type: 'sedan', amount: 8, price: 24195, onSale: false },
-//   { make: 'Mazda', model: 'CX-9', type: 'suv', amount: 7, price: 31520, onSale: true },
-//   { make: 'Toyota', model: '4Runner', type: 'suv', amount: 19, price: 34210, onSale: false },
-//   { make: 'Toyota', model: 'Sequoia', type: 'suv', amount: 16, price: 45560, onSale: false },
-//   { make: 'Toyota', model: 'Tacoma', type: 'truck', amount: 4, price: 24320, onSale: true },
-//   { make: 'Ford', model: 'F-150', type: 'truck', amount: 11, price: 27110, onSale: true },
-//   { make: 'Ford', model: 'Fusion', type: 'sedan', amount: 13, price: 22120, onSale: true },
-//   { make: 'Ford', model: 'Explorer', type: 'suv', amount: 6, price: 31660, onSale: false }
+//   {
+//     make: 'Honda',
+//     model: 'CR-V',
+//     type: 'suv',
+//     amount: 14,
+//     price: 24045,
+//     onSale: true,
+//   },
+//   {
+//     make: 'Honda',
+//     model: 'Accord',
+//     type: 'sedan',
+//     amount: 2,
+//     price: 22455,
+//     onSale: true,
+//   },
+//   {
+//     make: 'Mazda',
+//     model: 'Mazda 6',
+//     type: 'sedan',
+//     amount: 8,
+//     price: 24195,
+//     onSale: false,
+//   },
+//   {
+//     make: 'Mazda',
+//     model: 'CX-9',
+//     type: 'suv',
+//     amount: 7,
+//     price: 31520,
+//     onSale: true,
+//   },
+//   {
+//     make: 'Toyota',
+//     model: '4Runner',
+//     type: 'suv',
+//     amount: 19,
+//     price: 34210,
+//     onSale: false,
+//   },
+//   {
+//     make: 'Toyota',
+//     model: 'Sequoia',
+//     type: 'suv',
+//     amount: 16,
+//     price: 45560,
+//     onSale: false,
+//   },
+//   {
+//     make: 'Toyota',
+//     model: 'Tacoma',
+//     type: 'truck',
+//     amount: 4,
+//     price: 24320,
+//     onSale: true,
+//   },
+//   {
+//     make: 'Ford',
+//     model: 'F-150',
+//     type: 'truck',
+//     amount: 11,
+//     price: 27110,
+//     onSale: true,
+//   },
+//   {
+//     make: 'Ford',
+//     model: 'Fusion',
+//     type: 'sedan',
+//     amount: 13,
+//     price: 22120,
+//     onSale: true,
+//   },
+//   {
+//     make: 'Ford',
+//     model: 'Explorer',
+//     type: 'suv',
+//     amount: 6,
+//     price: 31660,
+//     onSale: false,
+//   },
 // ];
 
 //=====================================================================================//
@@ -21,7 +91,7 @@
 
 // Пусть функция getModels возвращает массив моделей (поле model) всех автомобилей.
 
-// const getModels = cars => {};
+// const getModels = cars => cars.map(car => car.model);
 
 // console.table(getModels(cars));
 
@@ -32,7 +102,13 @@
 // Пусть функция makeCarsWithDiscount возвращает новый массив объектов с изменным
 // значением свойства price в зависимости от переданной скидки.
 
-// const makeCarsWithDiscount = (cars, discount) => {};
+// const makeCarsWithDiscount = (cars, discount) =>
+//   cars.map(car => {
+//     return {
+//       ...car,
+//       newPrice: car.price - car.price * discount,
+//     };
+//   });
 
 // console.table(makeCarsWithDiscount(cars, 0.2));
 // console.table(makeCarsWithDiscount(cars, 0.4));
@@ -44,7 +120,8 @@
 // Пусть функция filterByPrice возвращает массив автомобилей цена которых меньше чем
 // значение параметра threshold.
 
-// const filterByPrice = (cars, threshold) => {};
+// const filterByPrice = (cars, threshold) =>
+//   cars.filter(({ price }) => price < threshold);
 
 // console.table(filterByPrice(cars, 30000));
 // console.table(filterByPrice(cars, 25000));
@@ -56,7 +133,7 @@
 // Пусть функция getCarsWithDiscount возвращает массив автомобилей свойство onSale
 // которых true.
 
-// const getCarsWithDiscount = cars => {};
+// const getCarsWithDiscount = cars => cars.filter(({ onSale }) => onSale);
 
 // console.table(getCarsWithDiscount(cars));
 
@@ -67,7 +144,7 @@
 // Пусть функция getCarsWithType возвращает массив автомобилей тип которых совпадает
 // со значением параметра type.
 
-// const getCarsWithType = (cars, type) => {};
+// const getCarsWithType = (cars, type) => cars.filter(car => car.type === type);
 
 // console.table(getCarsWithType(cars, 'suv'));
 // console.table(getCarsWithType(cars, 'sedan'));
@@ -76,7 +153,7 @@
 
 // Example 6 - Метод find
 
-// const getCarByModel = (cars, model) => {};
+// const getCarByModel = (cars, model) => cars.find(car => car.model === model);
 
 // console.log(getCarByModel(cars, 'F-150'));
 // console.log(getCarByModel(cars, 'CX-9'));
@@ -88,7 +165,8 @@
 // Пусть функция sortByAscendingAmount возвращает новый массив автомобилей
 // отсортированный по возврастанию значения свойства amount.
 
-// const sortByAscendingAmount = cars => {};
+// const sortByAscendingAmount = cars =>
+//   [...cars].sort((a, b) => a.amount - b.amount);
 
 // console.table(sortByAscendingAmount(cars));
 
@@ -99,7 +177,8 @@
 // Пусть функция sortByDescendingPrice возвращает новый массив автомобилей отсортированный
 // по убыванию значения свойства price.
 
-// const sortByDescendingPrice = cars => {};
+// const sortByDescendingPrice = cars =>
+//   [...cars].sort((a, b) => b.price - a.price);
 
 // console.table(sortByDescendingPrice(cars));
 
@@ -111,7 +190,12 @@
 // названию модели в алфавитном и обратном алфавитном порядке, в засисимости от значения
 // параметра order.
 
-// const sortByModel = (cars, order) => {};
+// const sortByModel = (cars, order) =>
+//   [...cars].sort((a, b) =>
+//     order === 'asc'
+//       ? a.model.localeCompare(b.model)
+//       : b.model.localeCompare(a.model)
+//   );
 
 // console.table(sortByModel(cars, 'asc'));
 // console.table(sortByModel(cars, 'desc'));
@@ -123,7 +207,8 @@
 // Пусть функция getTotalAmount возвращает общее количество
 // автомобилей(значение свойств amount).
 
-// const getTotalAmount = cars => {};
+// const getTotalAmount = cars =>
+//   cars.reduce((total, car) => total + car.amount, 0);
 
 // console.log(getTotalAmount(cars));
 
@@ -134,7 +219,8 @@
 // Пусть функция getAvailableCarNames возвращает массив моделей автомобилей, но только
 // тех, которые сейчас на распродаже.
 
-// const getModelsOnSale = cars => {};
+// const getModelsOnSale = cars =>
+//   cars.filter(({ onSale }) => onSale).map(({ model }) => model);
 
 // console.table(getModelsOnSale(cars));
 
@@ -145,7 +231,8 @@
 // Пусть функция getSortedCarsOnSale возвращает массив автомобилей
 // на распродаже(свойство onSale), отсортированных по возрастанию цены.
 
-// const getSortedCarsOnSale = cars => {};
+// const getSortedCarsOnSale = cars =>
+//   cars.filter(({ onSale }) => onSale).sort((a, b) => a.price - b.price);
 
 // console.table(getSortedCarsOnSale(cars));
 
